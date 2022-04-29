cdb1b45dba7562c3303294a4a0a0ca003aee9792
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-04-29 12:31:14 +0530

    n=int(input()) n1=0 n2=1 print(n1,n2,end=' ') for i in range (n-2):     a=n1+n2     n1=n2     n2=a     print(a,end=' ')

--- fibonacci.py
+++ fibonacci.py
@@ -2,8 +2,8 @@ n=int(input())
 n1=0
 n2=1
 print(n1,n2,end=' ')
-for i in range(n-2):
-    m=n1+n2
+for i in range (n-2):
+    a=n1+n2
     n1=n2
-    n2=m
-    print(m,end=' ')
+    n2=a
+    print(a,end=' ')
