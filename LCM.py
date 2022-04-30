201002d84c5cfdb36ae2ae19e16cfff8f608ab62
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-04-30 19:58:42 +0530

    n1,n2=map(int,input().split()) def compute_lcm(x, y):    if x > y:        greater = x    else:        greater = y
       while(True):
           if((greater % x == 0) and (greater % y == 0)):
               lcm = greater
               break
           greater += 1
    
       return lcm
    print(compute_lcm(n1, n2))

---
+++ LCM.py
@@ -0,0 +1,15 @@
+n1,n2=map(int,input().split())
+def compute_lcm(x, y):
+   if x > y:
+       greater = x
+   else:
+       greater = y
+
+   while(True):
+       if((greater % x == 0) and (greater % y == 0)):
+           lcm = greater
+           break
+       greater += 1
+
+   return lcm
+print(compute_lcm(n1, n2))
