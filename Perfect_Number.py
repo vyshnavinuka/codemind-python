5e3f51c55bfdf98d64308a182cb21f2631b28755
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-05-01 11:16:18 +0530

    n = int(input()) sum1 = 0 for i in range(1, n):     if(n % i == 0):         sum1 = sum1 + i if (sum1 == n):     print("True") else:     print("False")

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -1,9 +1,9 @@
-num=int(input())  
-sum=0  
-for i in range(1,num):  
-    if (num%i==0):  
-        sum=sum+i  
-if(sum==num):  
-    print("True")  
-else:  
-    print("False") 
+n = int(input())
+sum1 = 0
+for i in range(1, n):
+    if(n % i == 0):
+        sum1 = sum1 + i
+if (sum1 == n):
+    print("True")
+else:
+    print("False")
