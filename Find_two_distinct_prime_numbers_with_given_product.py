993ea876c39160990c0f0450b233b226f53292ad
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-29 12:56:29 +0530

    n=int(input()) a=[0]*n d=k=e=b=0 for i in range (2,n+1):     d=0     for j in range(2,i):         if i%j==0:             d=1             break     if d==0:         a[k]=i         k+=1 for i in range(k-1,-1,-1):     for j in range(k-1,-1,-1):         if n==a[i]*a[j]:             b=a[i]             e=a[j]             break if b==0 and e==0:     print('-1') else:     print(b,e)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -1,16 +1,22 @@
-def prime(n):
-    if n==1:
-        return 0
-    for i in range(2,int(n**0.5)+1):
-        if n%i==0:
-            return 0
-    return 1
 n=int(input())
-c=0
-for i in range(2,n):
-    if n%i==0:
-        if prime(i):
-            print(i,end=' ')
-            c+=1
-if c==0:
-    print("-1")
+a=[0]*n
+d=k=e=b=0
+for i in range (2,n+1):
+    d=0
+    for j in range(2,i):
+        if i%j==0:
+            d=1
+            break
+    if d==0:
+        a[k]=i
+        k+=1
+for i in range(k-1,-1,-1):
+    for j in range(k-1,-1,-1):
+        if n==a[i]*a[j]:
+            b=a[i]
+            e=a[j]
+            break
+if b==0 and e==0:
+    print('-1')
+else:
+    print(b,e)
