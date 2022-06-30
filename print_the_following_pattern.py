2cc4626320fa2a5222ff14aab217fa960dacd49a
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 20:05:25 +0530

    a=int(input()) for i in range(a):     for j in range(a-i-1):         print(" ",end="")     for j in range(a):         if i==0 or i==a-1:             print("*",end="")         else:             if j==0 or j==a-1:                 print("*",end="")             else:                 print(" ",end="")     print("")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,9 +1,13 @@
 a=int(input())
 for i in range(a):
-    for j in range(a-(i+1)):
+    for j in range(a-i-1):
         print(" ",end="")
-    for j in range(i,0,-1):
-        print(j,end="")
-    for j in range(i+1):
-        print(j,end="")
+    for j in range(a):
+        if i==0 or i==a-1:
+            print("*",end="")
+        else:
+            if j==0 or j==a-1:
+                print("*",end="")
+            else:
+                print(" ",end="")
     print("")
