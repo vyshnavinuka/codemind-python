3d5d192c4c4c3badccf78502093f8e6c8d19edf1
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 20:13:04 +0530

    a=int(input()) arr=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ") for i in range(a):     for j in range(a-(i+1)):         print(" ",end="")     for j in range(i+1):         print(arr[j],end="")     for j in range(i-1,-1,-1):         print(arr[j],end="")     print()

--- Print_the_following_pattern.py
+++ Print_the_following_pattern.py
@@ -1,9 +1,10 @@
 a=int(input())
+arr=list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
 for i in range(a):
-    for j in range(a):
-        if i==j or i+j==a-1:
-            print(a-i,end=" ")
-        else:
-            print("",end=" ")
+    for j in range(a-(i+1)):
+        print(" ",end="")
+    for j in range(i+1):
+        print(arr[j],end="")
+    for j in range(i-1,-1,-1):
+        print(arr[j],end="")
     print()
-        
