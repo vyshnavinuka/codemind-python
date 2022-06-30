d0957b1108918bebb8d802cea8b28382ffa742e4
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 20:16:38 +0530

    a=int(input()) for i in range(a):     for j in range(a-(i+1)):         print(" ",end="")     for j in range(i+1):         print(j+1,end="")     for j in range(i-1,-1,-1):         print(j+1,end="")     print("")

--- Pattern.py
+++ Pattern.py
@@ -1,5 +1,9 @@
-n=int(input())
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(i,end=' ')
-    print()
+a=int(input())
+for i in range(a):
+    for j in range(a-(i+1)):
+        print(" ",end="")
+    for j in range(i+1):
+        print(j+1,end="")
+    for j in range(i-1,-1,-1):
+        print(j+1,end="")
+    print("")
