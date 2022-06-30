19b9d34e083d968f23b9bdc5c1fcae5c52ea4712
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-06-30 20:00:45 +0530

    a=int(input()) for i in range(a):     for j in range(a-(i+1)):         print(" ",end="")     for j in range(i,0,-1):         print(j,end="")     for j in range(i+1):         print(j,end="")     print("")

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -2,6 +2,8 @@ a=int(input())
 for i in range(a):
     for j in range(a-(i+1)):
         print(" ",end="")
-    for j in range(i*2+1):
-        print(i+1,end="")
+    for j in range(i,0,-1):
+        print(j,end="")
+    for j in range(i+1):
+        print(j,end="")
     print("")
