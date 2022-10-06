e5d6d02626bf8f6209125d197552e34d74a1be39
vyshnavinuka <97425804+vyshnavinuka@users.noreply.github.com>
2022-10-06 17:23:06 +0530

    n=int(input()) arr=list(map(int,input().strip().split())) a,b=map(int,input().split()) min=999 c=0 for i in range(n):     if arr[i]<a or arr[i]>b:         c=c+1         if arr[i]<min:             min=arr[i] if c==0:     print(-1) else:     print(min)

---
+++ minimum_not_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+arr=list(map(int,input().strip().split()))
+a,b=map(int,input().split())
+min=999
+c=0
+for i in range(n):
+    if arr[i]<a or arr[i]>b:
+        c=c+1
+        if arr[i]<min:
+            min=arr[i]
+if c==0:
+    print(-1)
+else:
+    print(min)
